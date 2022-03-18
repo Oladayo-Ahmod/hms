4faf19076f9d116b0ef03e71cc861c42887dd6d0
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-18 20:51:20 +0100

    modify the packages clear

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,10 @@
     </body>
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="{{ asset('assets/js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{asset('assets/vendor/owl-carousel/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('assets/vendor/wow/wow.min.js')}}"></script>
+    <script src="../assets/js/theme.js"></script>
     <script src="{{ asset('assets/js/theme.js')}}"></script>
     <script src="{{ asset('assets/js/google-maps.js')}}"></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </html>
