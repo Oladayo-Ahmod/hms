d4077422c025e84ba0622cfeb377dfc64db18c95
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-18 23:51:47 +0100

    upgrade to vue 3

---
+++ resources/js/components/UsersComponents/About.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <Header />
+  <Footer />
+</div>
+
+</template>
+
+<script>
+import Header from './layouts/Header'
+import Footer from './layouts/Footer'
+export default {
+name : "About",
+components : {
+    Header,
+    Footer,
+},
+}
+</script>
+
+<style>
+
+</style>
