792b6966d054f35352415e49d4e06ca81108b2a8
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-18 20:39:28 +0100

    modify the packages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Hospital Management System</title>
         <link rel="stylesheet" href="{{asset('assets/css/maicons.css')}}">
-        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css')}}">
         <link rel="stylesheet" href="{{asset('assets/vendor/owl-carousel/css/owl.carousel.css')}}">
         <link rel="stylesheet" href="{{asset('assets/vendor/animate/animate.css')}}">
         <link rel="stylesheet" href="{{asset('assets/css/theme.css')}}">
@@ -18,5 +18,8 @@
         </div>
     </body>
     <script src="{{ mix('/js/app.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{ asset('assets/js/theme.js')}}"></script>
+    <script src="{{ asset('assets/js/google-maps.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </html>
