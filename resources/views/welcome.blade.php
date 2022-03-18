c8dd553fb91225198afd544531eae52a6477ae8e
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-18 18:57:11 +0100

    set up the welcome  blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Hospital Management System</title>
+        <link rel="stylesheet" href="{{asset('assets/css/maicons.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/vendor/owl-carousel/css/owl.carousel.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/vendor/animate/animate.css')}}">
+        <link rel="stylesheet" href="{{asset('assets/css/theme.css')}}">
 
     </head>
     <body class="antialiased">
@@ -13,5 +17,6 @@
             <home-component></home-component>
         </div>
     </body>
+    <script src="{{ mix('/js/app.js') }}"></script>
     <script src="{{ mix('/js/app.js') }}"></script>
 </html>
