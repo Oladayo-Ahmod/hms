db0b46e72adf06df260c79dce12e78a2384b001c
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-19 06:50:52 +0100

    bug fix clear

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ require('./bootstrap');
 import { createApp } from 'vue';
 import Home from './components/UsersComponents/Home';
 import VueRouter from 'vue-router'
-import router from './router/index'
+import router from './router'
 
 // createApp(App).use(router).mount('#app')
 createApp(Home).use(router).mount('#app')
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,5 +1,20 @@
-import routes from '../routes'
+import { createWebHistory, createRouter } from "vue-router";
+import Home from '../components/UsersComponents/Home'
+import About from '../components/UsersComponents/About'
+// import routes from '../routes'
+const routes = [
 
+  {
+      path : '/',
+      component : Home,
+      name : "home"
+  },
+  {
+      path : '/about',
+      component : About,
+      name : "about"
+  },
+]
 
 const router = createRouter({
     history: createWebHistory(),
