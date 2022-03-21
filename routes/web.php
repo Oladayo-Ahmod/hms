3d58745689a7efb8c362e3814b5988540ebe2732
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-21 14:37:01 +0100

    set routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
