63dd0653c36b19782cf6b4faa753053a3b128a33
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-22 14:06:20 +0100

    contact section

--- public/js/app.js
+++ public/js/app.js
@@ -22896,17 +22896,62 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "page-banner overlay-dark bg-image",
+  style: {
+    "background-image": "url(../assets/img/bg_image_1.jpg)"
+  }
+};
+var _hoisted_2 = {
+  "class": "banner-section"
+};
+var _hoisted_3 = {
+  "class": "container text-center wow fadeInUp"
+};
+var _hoisted_4 = {
+  "aria-label": "Breadcrumb"
+};
+var _hoisted_5 = {
+  "class": "breadcrumb breadcrumb-dark bg-transparent justify-content-center py-0 mb-2"
+};
+var _hoisted_6 = {
+  "class": "breadcrumb-item"
+};
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"page-banner overlay-dark bg-image\" style=\"background-image:url(../assets/img/bg_image_1.jpg);\"><div class=\"banner-section\"><div class=\"container text-center wow fadeInUp\"><nav aria-label=\"Breadcrumb\"><ol class=\"breadcrumb breadcrumb-dark bg-transparent justify-content-center py-0 mb-2\"><li class=\"breadcrumb-item\"><a href=\"index.html\">Home</a></li><li class=\"breadcrumb-item active\" aria-current=\"page\">Contact</li></ol></nav><h1 class=\"font-weight-normal\">Contact</h1></div><!-- .container --></div><!-- .banner-section --></div>", 1);
+var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Home");
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"page-section\"><div class=\"container\"><h1 class=\"text-center wow fadeInUp\">Get in Touch</h1><form class=\"contact-form mt-5\"><div class=\"row mb-3\"><div class=\"col-sm-6 py-2 wow fadeInLeft\"><label for=\"fullName\">Name</label><input type=\"text\" id=\"fullName\" class=\"form-control\" placeholder=\"Full name..\"></div><div class=\"col-sm-6 py-2 wow fadeInRight\"><label for=\"emailAddress\">Email</label><input type=\"text\" id=\"emailAddress\" class=\"form-control\" placeholder=\"Email address..\"></div><div class=\"col-12 py-2 wow fadeInUp\"><label for=\"subject\">Subject</label><input type=\"text\" id=\"subject\" class=\"form-control\" placeholder=\"Enter subject..\"></div><div class=\"col-12 py-2 wow fadeInUp\"><label for=\"message\">Message</label><textarea id=\"message\" class=\"form-control\" rows=\"8\" placeholder=\"Enter Message..\"></textarea></div></div><button type=\"submit\" class=\"btn btn-primary wow zoomIn\">Send Message</button></form></div></div><div class=\"maps-container wow fadeInUp\"><div id=\"google-maps\"></div></div><div class=\"page-section banner-home bg-image\" style=\"background-image:url(../assets/img/banner-pattern.svg);\"><div class=\"container py-5 py-lg-0\"><div class=\"row align-items-center\"><div class=\"col-lg-4 wow zoomIn\"><div class=\"img-banner d-none d-lg-block\"><img src=\"assets/img/mobile_app.png\" alt=\"\"></div></div><div class=\"col-lg-8 wow fadeInRight\"><h1 class=\"font-weight-normal mb-3\">Get easy access of all features using One Health Application</h1><a href=\"#\"><img src=\"assets/img/google_play.svg\" alt=\"\"></a><a href=\"#\" class=\"ml-2\"><img src=\"assets/img/app_store.svg\" alt=\"\"></a></div></div></div></div>", 3);
+var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("li", {
+  "class": "breadcrumb-item active",
+  "aria-current": "page"
+}, "Contact", -1
+/* HOISTED */
+);
+
+var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h1", {
+  "class": "font-weight-normal"
+}, "Contact", -1
+/* HOISTED */
+);
+
+var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"page-section\"><div class=\"container\"><h1 class=\"text-center wow fadeInUp\">Get in Touch</h1><form class=\"contact-form mt-5\"><div class=\"row mb-3\"><div class=\"col-sm-6 py-2 wow fadeInLeft\"><label for=\"fullName\">Name</label><input type=\"text\" id=\"fullName\" class=\"form-control\" placeholder=\"Full name..\"></div><div class=\"col-sm-6 py-2 wow fadeInRight\"><label for=\"emailAddress\">Email</label><input type=\"text\" id=\"emailAddress\" class=\"form-control\" placeholder=\"Email address..\"></div><div class=\"col-12 py-2 wow fadeInUp\"><label for=\"subject\">Subject</label><input type=\"text\" id=\"subject\" class=\"form-control\" placeholder=\"Enter subject..\"></div><div class=\"col-12 py-2 wow fadeInUp\"><label for=\"message\">Message</label><textarea id=\"message\" class=\"form-control\" rows=\"8\" placeholder=\"Enter Message..\"></textarea></div></div><button type=\"submit\" class=\"btn btn-primary wow zoomIn\">Send Message</button></form></div></div><div class=\"maps-container wow fadeInUp\"><div id=\"google-maps\"></div></div><div class=\"page-section banner-home bg-image\" style=\"background-image:url(../assets/img/banner-pattern.svg);\"><div class=\"container py-5 py-lg-0\"><div class=\"row align-items-center\"><div class=\"col-lg-4 wow zoomIn\"><div class=\"img-banner d-none d-lg-block\"><img src=\"assets/img/mobile_app.png\" alt=\"\"></div></div><div class=\"col-lg-8 wow fadeInRight\"><h1 class=\"font-weight-normal mb-3\">Get easy access of all features using One Health Application</h1><a href=\"#\"><img src=\"assets/img/google_play.svg\" alt=\"\"></a><a href=\"#\" class=\"ml-2\"><img src=\"assets/img/app_store.svg\" alt=\"\"></a></div></div></div></div>", 3);
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_Header = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Header");
 
+  var _component_router_link = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("router-link");
+
   var _component_Footer = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Footer");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Header), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .page-banner "), _hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .banner-home "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Footer)], 64
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Header), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("nav", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ol", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("li", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_router_link, {
+    to: "/"
+  }, {
+    "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
+      return [_hoisted_7];
+    }),
+    _: 1
+    /* STABLE */
+
+  })]), _hoisted_8])]), _hoisted_9]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .container ")]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .banner-section ")]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .page-banner "), _hoisted_10, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" .banner-home "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Footer)], 64
   /* STABLE_FRAGMENT */
   );
 }
