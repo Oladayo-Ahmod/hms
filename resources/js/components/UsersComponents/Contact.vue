457181bdc5f9ef520c89d1d4570658dc953010b2
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-22 12:54:01 +0100

    contact section

---
+++ resources/js/components/UsersComponents/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <Header />
+
+  <Footer />
+</template>
+
+<script>
+
+import Footer from './layouts/Footer.vue'
+import Header from './layouts/Header.vue'
+export default {
+    name : "Contact",
+    components : {
+        Footer,
+        Header
+    },
+}
+</script>
+
+<style>
+
+</style>
