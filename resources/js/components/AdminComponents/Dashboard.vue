93532c31fb40326cd53619941d6fa9203b0a5b77
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-22 15:42:43 +0100

    admin dashboard funcionality

--- resources/js/components/AdminComponents/Dashboard.vue
+++ resources/js/components/AdminComponents/Dashboard.vue
@@ -1,12 +1,17 @@
 <template>
-  
+  <Header />
+
+  <Footer />
 </template>
 
 <script>
+import Header from './layouts/Header.vue'
+import Footer from './layouts/Footer.vue'
 export default {
     name : "Dashboard",
     components : {
-        
+        Header,
+        Footer
     }
 }
 </script>
