6864d29b608f0a03d4a57defed43795f54a56f20
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-03-22 12:44:17 +0100

    doctors section

--- resources/js/components/UsersComponents/Doctors.vue
+++ resources/js/components/UsersComponents/Doctors.vue
@@ -1,5 +1,18 @@
 <template>
   <Header />
+    <div class="page-banner overlay-dark bg-image" style="background-image: url(../assets/img/bg_image_1.jpg);">
+    <div class="banner-section">
+      <div class="container text-center wow fadeInUp">
+        <nav aria-label="Breadcrumb">
+          <ol class="breadcrumb breadcrumb-dark bg-transparent justify-content-center py-0 mb-2">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Doctors</li>
+          </ol>
+        </nav>
+        <h1 class="font-weight-normal">Our Doctors</h1>
+      </div> <!-- .container -->
+    </div> <!-- .banner-section -->
+  </div> <!-- .page-banner -->
       <div class="page-section bg-light">
     <div class="container">
       <div class="row justify-content-center">
