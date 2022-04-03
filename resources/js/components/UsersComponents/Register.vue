a704b9b4b25f011ca3c18d3da432c1ab55a1f2c3
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-03 23:06:22 +0100

    user registration form

---
+++ resources/js/components/UsersComponents/Register.vue
@@ -0,0 +1,35 @@
+<template>
+    <Header />
+          <div class="page-banner overlay-dark bg-image" style="background-image: url(../assets/img/bg_image_1.jpg);">
+    <div class="banner-section">
+      <div class="container text-center wow fadeInUp">
+        <nav aria-label="Breadcrumb">
+          <ol class="breadcrumb breadcrumb-dark bg-transparent justify-content-center py-0 mb-2">
+            <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
+            <li class="breadcrumb-item active" aria-current="page">Registration Form</li>
+          </ol>
+        </nav>
+        <h1 class="font-weight-normal">Contact</h1>
+      </div> <!-- .container -->
+    </div> <!-- .banner-section -->
+  </div> <!-- .page-banner -->
+
+    <Footer />
+</template>
+
+<script>
+
+import Footer from './layouts/Footer.vue'
+import Header from './layouts/Header.vue'
+export default {
+    name : "Register",
+    components : {
+        Footer,
+        Header
+    },
+}
+</script>
+
+<style>
+
+</style>
