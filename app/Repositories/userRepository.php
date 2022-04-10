5949f70b3377706f612e55a56b216cc1840fb636
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-10 16:05:07 +0100

    set up user repository

--- app/Repositories/userRepository.php
+++ app/Repositories/userRepository.php
@@ -9,12 +9,33 @@ class UserRepository implements UserInterface{
     public function getAllUsers()
     {
         $users = User::all();
-        return response()->json(['allUsers'=>$users]);
+        return $users;
     }
 
     // get user by Id
     public function getUserById($userId)
     {
         $user = User::findOrFail($userId);
-    } 
+        return $user;
+    }
+
+    // create new user 
+    public function createUser(array $userDetails)
+    {
+        $user = User::create($userDetails);
+        return $user;
+    }
+
+    // update user
+    public function updateUser($id, array $newUserDetails)
+    {
+        $user = User::findOrFail($id);
+        $user->update($newUserDetails);
+    }
+
+    // delete user
+    public function deleteUser($id)
+    {
+        User::destroy($id);
+    }
 }
